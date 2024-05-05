5d383a51c4663c9144cd89dcb4a3f15ba5c3b7a0
hadeer-khaled <hadeer.awad02@eng-st.cu.edu.eg>
2024-05-05 21:17:13 +0300

    .

--- laravel-api/routes/api.php
+++ laravel-api/routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\PostController ;
-
+ 
 /*
 |--------------------------------------------------------------------------
 | API Routes
